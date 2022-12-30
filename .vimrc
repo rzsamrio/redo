5b5d6989c32fbf86eb6039561d6c8395329d688c
rzsamrio <samuelekop144@gmail.com>
2022-12-29 21:18:12 -0800

    Project redo: credit - Commit 1

---
+++ .vimrc
@@ -0,0 +1,15 @@
+" Start in command mode
+set ambw=double
+
+" Activate the mouse
+set mouse=a
+
+" Set Tab
+set smartindent
+set noexpandtab
+set tabstop=4
+set shiftwidth=4
+
+"Set theme
+syntax on
+colorscheme codedark
--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,47 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool checkluhn(long number, int length);
+
 int main(void) 
 {
-	int card_number = get_int("Number: ");	
+	long card_number = get_long("Number: ");
+
+	// Card length validity
+
+	// Return card length
+	int card_length = 0;
+	long clone_no = card_number;
+	for (int i = 0; clone_no != 0; i++, card_length++)
+		clone_no /= 10;
+	
+	// Verify Conditions
+	// AMEX?
+	checkluhn(card_number, card_length);
+
+	return 0;
 }
+
+bool checkluhn(long number, int length)
+{
+	// Store card number in array
+	int num[20];
+	for (int temp = 0, l = length; l > 0; l--)
+	{
+		temp = number % 10;
+		number /= 10;
+		num[l - 1] = temp;
+	}
+	
+	// Calculate
+	int msum = 0; // Sum of the multiplied
+	for (int l = length - 2; l > 0; l -=2)
+	{
+		// To-do
+		// Sum of the multiplied number digits
+		msum += num[l] * 2;
+		printf("%i ", msum);
+	}
+	printf("\n%i\n", msum);
+}
+
