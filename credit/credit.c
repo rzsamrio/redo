c9a4d6d2895b186bf878ef633db3aa9621693dd0
rzsamrio <samuelekop144@gmail.com>
2022-12-30 13:02:35 -0800

    redo Project 2: Credit completed

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
-bool checkluhn(long number, int length);
+bool checkluhn(int length);
+int num[20];
 
 int main(void) 
 {
@@ -12,36 +13,74 @@ int main(void)
 	// Return card length
 	int card_length = 0;
 	long clone_no = card_number;
-	for (int i = 0; clone_no != 0; i++, card_length++)
+	while (clone_no != 0)
+	{
 		clone_no /= 10;
-	
+		card_length++;
+	}
+
+	for (int temp = 0, l = card_length; l > 0; l--)
+	{
+		temp = card_number % 10;
+		card_number /= 10;
+		num[l - 1] = temp;
+	}
+
+
 	// Verify Conditions
 	// AMEX?
-	checkluhn(card_number, card_length);
+	if (card_length == 15 && num[0] == 3 && (num[1] == 4 || num[1] == 7))
+	{
+		if (checkluhn(card_length))
+		{
+			printf("AMEX\n");
+			return 0;
+		}
+	}
 
+	// Visa?
+	if ((card_length == 13 || card_length == 16) && num[0] == 4)
+	{
+		if (checkluhn(card_length))
+		{
+			printf("VISA\n");
+			return 0;
+		}
+	}
+
+	//MasterCard?
+	if (card_length == 16 && num[0] == 5 && num[1] < 6 && num[1] > 0)
+	{
+		if (checkluhn(card_length))
+		{
+			printf("MASTERCARD\n");
+			return 0;
+		}
+	}
+	
+	printf("INVALID\n");
 	return 0;
 }
 
-bool checkluhn(long number, int length)
+bool checkluhn(int length)
 {
-	// Store card number in array
-	int num[20];
-	for (int temp = 0, l = length; l > 0; l--)
-	{
-		temp = number % 10;
-		number /= 10;
-		num[l - 1] = temp;
-	}
-	
 	// Calculate
 	int msum = 0; // Sum of the multiplied
-	for (int l = length - 2; l > 0; l -=2)
+	int asum = 0; // Sum of the unmultplied
+	int tsum = 0; // Total sum
+	for (int l = length - 2, temp = 0; l >= 0; l -=2)
 	{
-		// To-do
-		// Sum of the multiplied number digits
-		msum += num[l] * 2;
-		printf("%i ", msum);
+		temp = num[l] * 2;
+		msum += (temp % 10) + (temp / 10); // multiplied units are tens
 	}
-	printf("\n%i\n", msum);
+	for (int i = length - 1; i >= 0; i -=2)
+		asum += num[i];
+	tsum = asum + msum;
+
+	// Return
+	if (tsum % 10 == 0)
+		return true;
+	else
+		return false;
 }
 
